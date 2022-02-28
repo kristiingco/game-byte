ac52a4166c186c4a9f379c9ecc2d477dc9dd9acc
Kristi <kristiana.d.ingco@gmail.com>
2022-02-27 17:50:57 -0800

    Add sign in functionality

--- src/components/sign-in/SignIn.jsx
+++ src/components/sign-in/SignIn.jsx
@@ -6,7 +6,7 @@ import "./SignIn.scss";
 import FormInput from "../form-input/FormInput";
 import CustomButton from "../custom-button/CustomButton";
 
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 class SignIn extends Component {
     constructor(props) {
@@ -18,13 +18,20 @@ class SignIn extends Component {
         };
     }
 
-    handleSubmit = (event) => {
+    handleSubmit = async (event) => {
         event.preventDefault();
 
-        this.setState = {
-            email: "",
-            password: "",
-        };
+        const { email, password } = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState = {
+                email: "",
+                password: "",
+            };
+        } catch (error) {
+            console.log(error);
+        }
     };
 
     handleChange = (event) => {
